43d4664ed0026017a2f4c5a856aa838ab7383f7f
Nathan Stepper <nstepper@uw.edu>
2023-03-30 16:37:37 -0700

    made some final changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,50 +1,50 @@
 // click event for the save button
 $(".saveBtn").on("click", function () {
+  // Get the user input
+  var description = $(this).siblings(".description").val().trim();
 
-    // Get the user input
-    var description = $(this).siblings(".description").val().trim();
+  if (description) {
     // Get the id of the user input textarea
-    const element = document.querySelector('.description');
-    const id = element.id;
-    
-     // Save the user input in local storage
+    var id = $(this).siblings(".description").attr("id"); 
+    // Save the user input in local storage
     localStorage.setItem(id, description);
+  }
+ 
+});
+
+// Apply the past, present, or future class to each time block
+function updateHourClasses() {
+  var currentHour = dayjs().hour(); // Get the current hour in 24-hour time
+  $(".time-block").each(function () {
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+    if (hour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (hour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
   });
+}
 
-  // Apply the past, present, or future class to each time block
-  function updateHourClasses() {
-    var currentHour = dayjs().hour(); // Get the current hour in 24-hour time
-    $(".time-block").each(function () {
-      var hour = parseInt($(this).attr("id").split("-")[1]);
-      if (hour < currentHour) {
-        $(this).removeClass("present future").addClass("past");
-      } else if (hour === currentHour) {
-        $(this).removeClass("past future").addClass("present");
-      } else {
-        $(this).removeClass("past present").addClass("future");
-      }
-    });
-  }
+// Get any user input that was saved in localStorage and set the values of
+// the corresponding textarea elements
+function loadSavedData() {
+  $(".time-block").each(function () {
+    var id = $(this).attr("id");
+    var description = localStorage.getItem(id);
+    if (description) {
+      $(this).find(".description").val(description);
+    }
+  });
+}
 
-  // Get any user input that was saved in localStorage and set the values of
-  // the corresponding textarea elements
-  function loadSavedData() {
-    $(".time-block").each(function () {
-      var id = $(this).attr("id");
-      var description = localStorage.getItem(id);
-      if (description) {
-        $(this).find(".description").val(description);
-      }
-    });
-  }
+// Display the current date in the header of the page
+$("#currentDay").text(dayjs().format("dddd, MMMM D"));
 
-  // Display the current date in the header of the page
-  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+// Call the updateHourClasses and loadSavedData functions
+updateHourClasses();
+loadSavedData();
 
-  // Call the updateHourClasses and loadSavedData functions
-  updateHourClasses();
-  loadSavedData();
-
-  // Update the hour classes every minute
-  setInterval(updateHourClasses, 60000);
-
+// Update the hour classes every minute
+setInterval(updateHourClasses, 60000);
